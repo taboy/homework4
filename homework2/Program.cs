24b7e84e2ab103f06a85c233c610639b0a81abea
a a <t.mirzeyev@mail.com>
2022-02-17 10:58:54 +0300

    finish

---
+++ homework2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 28: Подсчитайте сумму цифр в числе.
+
+Console.WriteLine("write a number "); 
+int sum = Convert.ToInt32(Console.ReadLine()); 
+            int a = 0; 
+            while (sum > 0) 
+            { 
+  
+                a = a + sum % 10; 
+                sum =sum /10 ; 
+  
+            } 
+ Console.WriteLine(a);

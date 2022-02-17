537d2d2516d2e1bf3e9c60d061d956a9871b924a
a a <t.mirzeyev@mail.com>
2022-02-17 10:55:39 +0300

    finish

---
+++ homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 26: Возведите число А в натуральную степень B, используя цикл.
+
+
+Console.WriteLine("write a number");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("write a stepen");
+int num2 = Convert.ToInt32(Console.ReadLine());
+void stepen(int a,int b){
+    int count=2;
+    int c=a;
+    while(count<=b){
+        a=a*c;
+        count++;
+        
+    }
+Console.WriteLine(a);
+
+}
+stepen(num,num2);

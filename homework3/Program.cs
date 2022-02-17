bfa1853da9b2058d6a7e5d082c50319dcef5f612
a a <t.mirzeyev@mail.com>
2022-02-17 13:07:20 +0300

    finish

---
+++ homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 30: Покажите кубы чисел, заканчивающихся на чётную цифру.
+
+Console.WriteLine("write a number");
+int num= Convert.ToInt16(Console.ReadLine());
+void kub(int num2){
+
+    int kub1 = num2;
+    int first=2;
+    int first1=1;
+    while(first<=num2){     
+        first1=first*first*first;
+        first++;
+        if (first1%2==0){
+            Console.WriteLine(first1);
+        }}}
+
+even(num);
